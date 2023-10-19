2b3d21c095343723f14f619e96d4cde112ba2342
TheBaytur <bayturcodding@gmail.com>
2023-10-19 20:08:44 +0800

    Second activity

--- app/src/main/java/com/example/a3/SecondActivity.java
+++ app/src/main/java/com/example/a3/SecondActivity.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
 
-    public void startNewActivity(View v){
-        Intent intent = new Intent(this, SecondActivity.class);
+    public void goBack(View v){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
     }
